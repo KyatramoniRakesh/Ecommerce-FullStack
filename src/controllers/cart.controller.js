ce9b05314277cdaeeb7c6303c49692d4ecadc6d5
KyatramoniRakesh <rakesh.rk1306@gmail.com>
2026-02-07 18:06:38 +0530

    Completed cart module with CRUD operations and backend cart totals

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -37,4 +37,119 @@ const addToCart = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
-module.exports = {addToCart}
+
+
+const getMyCart = async (req, res) => {
+  try {
+    const userId = req.user.id
+
+    const cart = await Cart.findOne({ user: userId })
+    .populate("items.product")
+
+    if (!cart || cart.items.length === 0) {
+      return res.status(200).json({
+        message: "Cart is empty",
+        items: [],
+        subtotal: 0
+      })
+    }
+    let subtotal = 0
+
+    const items = cart.items.map((item) => {
+      const itemTotal = item.product.price * item.quantity
+      subtotal += itemTotal
+
+      return {
+        product: item.product,
+        quantity: item.quantity,
+        itemTotal
+      }
+    })
+
+    res.status(200).json(
+      items,
+      subtotal)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+
+const updateQuantity = async (req, res) => {
+  try {
+    const userId = req.user.id
+    const { productId, quantity } = req.body
+
+    if (quantity < 0) {
+      return res.status(400).json({ message: "Quantity cannot be negative" })
+    }
+
+    const cart = await Cart.findOne({ user: userId })
+    if (!cart) {
+      return res.status(200).json({ items: [] })
+    }
+
+    const itemIndex = cart.items.findIndex(
+      (item) => item.product.toString() === productId
+    )
+
+    if (itemIndex === -1) {
+      return res.status(404).json({ message: "Product not in cart" })
+    }
+
+    // quantity == 0 → remove item
+    if (quantity === 0) {
+      cart.items.splice(itemIndex, 1)
+    } else {
+      cart.items[itemIndex].quantity = quantity
+    }
+
+    await cart.save()
+
+    res.status(200).json({
+      message: "Cart updated",
+      cart
+    })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+const removeItem = async (req, res) => {
+  try {
+    const userId = req.user.id
+    const { productId } = req.params
+
+    const cart = await Cart.findOne({ user: userId })
+    if (!cart) {
+      return res.status(200).json({ items: [] })
+    }
+
+    const initialLength = cart.items.length
+
+    cart.items = cart.items.filter(
+      (item) => item.product.toString() !== productId
+    )
+
+    if (cart.items.length === initialLength) {
+      return res.status(404).json({ message: "Product not in cart" })
+    }
+
+    await cart.save()
+
+    res.status(200).json({
+      message: "Item removed from cart",
+      cart
+    })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+module.exports = {
+  addToCart,
+  getMyCart,
+  updateQuantity,
+  removeItem
+}
+
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
-const { addToCart } = require("../controllers/cart.controller")
+const { addToCart, getMyCart,updateQuantity , removeItem} = require("../controllers/cart.controller")
 const { protect } = require("../middleware/authMiddlewares")
 
 router.post("/add", protect, addToCart)
+router.get("/", protect, getMyCart)
+router.put("/update", protect, updateQuantity)
+router.delete("/remove/:productId", protect, removeItem)
 
 module.exports = router
